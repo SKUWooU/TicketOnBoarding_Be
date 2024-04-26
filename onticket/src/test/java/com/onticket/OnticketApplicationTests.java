8e7bbd2c4a555bec27069cfabc4189e657e96cfb
박건우 <parkernew@baggeon-uui-MacBookPro.local>
2024-04-26 14:05:22 +0900

    init-setting file

---
+++ onticket/src/test/java/com/onticket/OnticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnticketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
