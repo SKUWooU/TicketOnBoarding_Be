6de1ba4a94f26e4eea85a6117c7b451aa7a1b095
박건우 <parkwoo9901@gmail.com>
2024-06-01 15:17:59 +0900

    FEAT: 날짜별 데이터 DTO

--- onticket/src/main/java/com/onticket/concert/dto/CalDto.java
+++ onticket/src/main/java/com/onticket/concert/dto/CalDto.java
@@ -1,2 +1,33 @@
-package com.onticket.concert.dto;public class CalDto {
+package com.onticket.concert.dto;
+
+import com.onticket.concert.domain.Seat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CalDto {
+    private Long id;
+
+    //날짜
+    private LocalDate date;
+
+    //요일
+    private String dayOfWeek;
+
+    //공연시간
+    private LocalTime startTime;
+
+    //좌석수
+    private int seatAmount;
+
+    private List<Seat> seatList;
 }
--- onticket/src/main/java/com/onticket/concert/dto/SeatDto.java
+++ onticket/src/main/java/com/onticket/concert/dto/SeatDto.java
@@ -1,2 +1,20 @@
-package com.onticket.concert.dto;public class seatDto {
+package com.onticket.concert.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SeatDto {
+    private Long id;
+
+    // 좌석 번호
+    private String seatNumber;
+
+    // 예약 상태
+    private boolean reserved;
 }
