53db2ab07734e874d44ec5982e8f820f383ae673
박건우 <parkwoo9901@gmail.com>
2024-06-01 15:19:56 +0900

    FEAT: 해당 날짜와 일치하는 데이터 있는지 검사

--- onticket/src/main/java/com/onticket/concert/repository/ConcertTimeRepository.java
+++ onticket/src/main/java/com/onticket/concert/repository/ConcertTimeRepository.java
@@ -1,7 +1,14 @@
 package com.onticket.concert.repository;
+import com.onticket.concert.domain.Concert;
 import com.onticket.concert.domain.ConcertTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ConcertTimeRepository extends JpaRepository<ConcertTime,Long> {
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
 
+public interface ConcertTimeRepository extends JpaRepository<ConcertTime,Long> {
+    List<ConcertTime> findByConcert_ConcertId(String concertId);
+    Optional<ConcertTime> findByDateAndStartTimeAndConcert(LocalDate date, LocalTime startTime, Concert concert);
 }
