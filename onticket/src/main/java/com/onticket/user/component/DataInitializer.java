64cdba97d413572c9d88c97d723bbd50a0c6aa22
박건우 <parkwoo9901@gmail.com>
2024-06-11 16:10:08 +0900

    feat: 임의 사용자 admin-adminpassword

--- onticket/src/main/java/com/onticket/user/component/DataInitializer.java
+++ onticket/src/main/java/com/onticket/user/component/DataInitializer.java
@@ -1,2 +1,31 @@
-package com.onticket.user.component;public class DataInitializer {
+package com.onticket.user.component;
+
+import com.onticket.user.domain.SiteUser;
+import com.onticket.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+
+@RequiredArgsConstructor
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (userRepository.findByUsername("admin") == null) {
+            SiteUser admin = new SiteUser();
+            admin.setUsername("admin");
+            admin.setEmail("admin@example.com");
+            admin.setNickname("Administrator");
+            admin.setPhonenumber("01068549901");
+            admin.setPassword(passwordEncoder.encode("adminpassword"));
+            userRepository.save(admin);
+        }
+    }
 }
