c95ff6af71a5facef3a58bdd5d8a4a336879c083
박건우 <parkwoo9901@gmail.com>
2024-06-21 14:29:51 +0900

    feat: 전체 예약 조회

--- onticket/src/main/java/com/onticket/admin/AdminController.java
+++ onticket/src/main/java/com/onticket/admin/AdminController.java
@@ -167,4 +167,16 @@ public class AdminController {
             return ResponseEntity.ok("회원을 삭제했습니다.");
         }else return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인이 필요한 서비스입니다.");
     }
+
+    @GetMapping("/admin/reservationlist")
+    public ResponseEntity<?> getReservationList(@CookieValue(value = "acceeToken", required = false) String token){
+        if (token != null && jwtUtil.validateToken(token)) {
+            String username = jwtUtil.getUsernameFromToken(token);
+            SiteUser user = userRepository.findByUsername(username);
+            if (user.getCode() != 3) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("관리자 권한이 없습니다.");
+            }
+            return ResponseEntity.ok(reservationRepository.findAll());
+        }else return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인이 필요한 서비스입니다.");
+    }
 }
