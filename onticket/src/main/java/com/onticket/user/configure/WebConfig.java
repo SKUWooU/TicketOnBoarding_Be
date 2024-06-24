d171b9572b2459739430761f588603adae1f9347
박건우 <parkwoo9901@gmail.com>
2024-06-24 09:36:53 +0900

    fix: nginx 에서 cors 관련 처리하고 들어오기 때문에 중복 발생

--- onticket/src/main/java/com/onticket/user/configure/WebConfig.java
+++ onticket/src/main/java/com/onticket/user/configure/WebConfig.java
@@ -1,27 +1,28 @@
-package com.onticket.user.configure;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-
-//CORS 설정
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*")
-                        .allowCredentials(true)
-                        .maxAge(3600);
-            }
-        };
-    }
-}
+//package com.onticket.user.configure;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//
+////CORS 설정
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedOrigins("https://ticket-on-boarding-qpredsscm-skuproject.vercel.app")
+//                        //http://localhost:5173
+//                        //https://ticket-on-boarding-fe.vercel.app
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                        .allowedHeaders("*")
+//                        .allowCredentials(true);
+//            }
+//        };
+//    }
+//}
